f27fe0f39db1e0e58d379bd1292023d36e7e8cd6
aayushbhati <124025774+aayushbhati@users.noreply.github.com>
2024-01-22 13:56:55 +0530

    agriculture

---
+++ thankyou.js
@@ -0,0 +1,9 @@
+<div class="AW-Form-36129367"></div>
+<script type="text/javascript">(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//forms.aweber.com/form/67/36129367.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, "script", "aweber-wjs-z2idovs7o"));
+</script>
